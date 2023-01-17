639c6e9ab002a246d1c8ab359eab02c53cde5e2a
Noorullah <shaiknooru247@gmail.com>
2023-01-18 03:33:58 +0530

    first commit scene setup

--- src/components/layout/canvas.tsx
+++ src/components/layout/canvas.tsx
@@ -1,32 +1,121 @@
-import { useRef } from "react";
-import { Canvas } from "@react-three/fiber";
-import { A11yAnnouncer } from "@react-three/a11y";
-import { OrbitControls, Preload, Stats } from "@react-three/drei";
+/** @format */
+
+import { useRef, useState } from "react"
+import { Canvas } from "@react-three/fiber"
+import { A11yAnnouncer } from "@react-three/a11y"
+import {
+	Effects,
+	Environment,
+	Lightformer,
+	OrbitControls,
+	PerspectiveCamera,
+	Preload,
+	Stats,
+} from "@react-three/drei"
+import Model from "../canvas/Perfume"
+import {
+	EffectComposer,
+	DepthOfField,
+	Bloom,
+	Noise,
+	Vignette,
+	SSR,
+	BrightnessContrast,
+	ToneMapping,
+} from "@react-three/postprocessing"
 
 const Controls = () => {
-  const control = useRef(null);
-  return <OrbitControls ref={control} />;
-};
+	const control = useRef(null)
+	return <OrbitControls ref={control} />
+}
 const CanvasWrapper = ({ children }) => {
-  return (
-    <>
-      <Canvas
-        // Is this deprecated or typed wrong? Ignoring for now.
-        // @ts-ignore
-        mode="concurrent"
-        style={{
-          position: "absolute",
-          top: 0,
-        }}
-      >
-        <Stats />
-        <Controls />
-        <Preload all />
-        {children}
-      </Canvas>
-      <A11yAnnouncer />
-    </>
-  );
-};
+	const [mCoord, setMCoord] = useState({
+		x: 0,
+		y: 0,
+	})
 
-export default CanvasWrapper;
+	const [isMouse, setIsMouse] = useState(false)
+
+	const canvas = useRef(null)
+	// console.log(canvas.current?.clientWidth)
+
+	const handlePointerMove = (e: any) => {
+		if (isMouse == true && canvas.current) {
+			let w = canvas.current?.clientWidth
+			let h = canvas.current?.clientHeight
+			let x = e.pageX
+			let y = e.pageY
+
+			x = (x - w / 2) / w
+			y = (y - h / 2) / h
+
+			setMCoord({
+				x: x,
+				y: y,
+			})
+		}
+	}
+
+	return (
+		<>
+			<Canvas
+				// Is this deprecated or typed wrong? Ignoring for now.
+				ref={canvas}
+				// @ts-ignore
+				mode="concurrent"
+				style={{
+					position: "absolute",
+					top: 0,
+					// background: "green",
+				}}
+				gl={{
+					powerPreference: "high-performance",
+					alpha: false,
+					antialias: false,
+					stencil: false,
+					depth: false,
+				}}
+				// @ts-ignore
+				onPointerOver={() => setIsMouse(true)}
+				// @ts-ignore
+				onPointerMove={handlePointerMove}
+				// @ts-ignore
+				onPointerLeave={() => setIsMouse(false)}
+			>
+				<color
+					attach="background"
+					args={["#283846"]}
+				/>
+				<Stats />
+				<Model mCoords={mCoord} />
+				<EffectComposer
+					multisampling={10}
+					disableNormalPass={true}
+				>
+					<Bloom
+						luminanceThreshold={0.05}
+						luminanceSmoothing={0.9}
+						height={200}
+						opacity={0.2}
+					/>
+					<DepthOfField
+						focusDistance={0.007}
+						focalLength={0.02}
+						bokehScale={1.1}
+						height={480}
+					/>
+					<BrightnessContrast
+						brightness={-0.05} // brightness. min: -1, max: 1
+						contrast={0.08} // contrast: min -1, max: 1
+					/>
+				</EffectComposer>
+				{/* <Controls /> */}
+				<Preload all />
+				{children}
+			</Canvas>
+			<A11yAnnouncer />
+		</>
+	)
+}
+
+export default CanvasWrapper
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,56 @@
-import useStore from "@/helpers/store";
-import dynamic from "next/dynamic";
-import { useRouter } from "next/router";
+/** @format */
+
+// import Model from "@/components/canvas/Perfume"
+import useStore from "@/helpers/store"
+import dynamic from "next/dynamic"
+import { useRouter } from "next/router"
 // import Shader from '@/components/canvas/ShaderExample/ShaderExample'
 
 // Prefer dynamic import for production builds
 // But if you have issues and need to debug in local development
 // comment these out and import above instead
 // https://github.com/pmndrs/react-three-next/issues/49
-const Shader = dynamic(
-  () => import("@/components/canvas/ShaderExample/ShaderExample"),
-  {
-    ssr: false,
-  }
-);
+const Model = dynamic(
+	() => import("@/components/canvas/Perfume"),
+	{
+		ssr: false,
+	}
+)
 
 // DOM elements here
 const DOM = () => {
-  return <></>;
-};
+	return (
+		<>
+			<h1>Hello world</h1>
+		</>
+	)
+}
 
 // Canvas/R3F components here
 const R3F = () => {
-  // Example of using the router to change pages
-  // It can also be inside R3F component (see `two.tsx` and `Box.tsx`)
-  const { router } = useStore();
-  const handleOnClick = () => {
-    router.push("/two");
-  };
+	// Example of using the router to change pages
+	// It can also be inside R3F component (see `two.tsx` and `Box.tsx`)
+	const { router } = useStore()
+	const handleOnClick = () => {
+		router.push("/two")
+	}
 
-  return (
-    <>
-      <Shader onClick={handleOnClick} />
-    </>
-  );
-};
+	return <></>
+}
 
 export default function Page() {
-  return (
-    <>
-      <DOM />
-      <R3F />
-    </>
-  );
+	return (
+		<>
+			<DOM />
+			<R3F />
+		</>
+	)
 }
 
 export async function getStaticProps() {
-  return {
-    props: {
-      title: "Welcome!",
-    },
-  };
+	return {
+		props: {
+			title: "Welcome!",
+		},
+	}
 }
